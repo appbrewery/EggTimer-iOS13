f06609ad152a92c732e75a900779ebb67fe38b45
Angela <8798027+angelabauer@users.noreply.github.com>
2019-07-08 11:58:39 +0100

    Remove viewDidLoad()

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,11 +10,5 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
 
