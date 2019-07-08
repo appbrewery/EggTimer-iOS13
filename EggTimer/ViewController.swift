72da619c6fc407c0b92cbfaea074bc8e8173358d
Angela <8798027+angelabauer@users.noreply.github.com>
2019-07-08 09:13:36 +0100

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
