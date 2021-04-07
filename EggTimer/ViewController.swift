4b84e3cad4dc4bac082f99bba612704a5b078801
Mikita Shah <mshah2511@gmail.com>
2021-04-07 23:12:02 +1000

    Revert "Adding progress bar and updating progress time for each type of egg"
    This reverts commit dbe31cf4bb074d6061a02f63119fc876294a817f.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,39 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Initialising a dictionary
-//    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    let eggTimes = ["Soft": 5, "Medium": 10, "Hard": 15]
-    var count = 0
-    var totalTimerDuration = 0
-    var timer = Timer()
-    
-    @IBOutlet weak var timerStatus: UILabel!
-    @IBOutlet weak var timerProgress: UIProgressView!
-    
-    @IBAction func hardnessSelected(_ sender: UIButton) {
-        timer.invalidate()
-        count = 0
-        timerProgress.progress = 0.0
-        
-        let hardness = sender.currentTitle! // Converting from Optional to String
-        timerStatus.text = hardness
-        
-        totalTimerDuration = eggTimes[hardness]!
-        
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
 
-    }
-    
-    @objc func update() {
-        if(count <= totalTimerDuration) {
-            timerProgress.progress = Float(count)/Float(totalTimerDuration)
-            count += 1
-        } else {
-            timer.invalidate()
-            timerStatus.text = "Done!"
-        }
-    }
-    
 
 }
